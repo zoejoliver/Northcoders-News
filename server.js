cd80239da83ca5cd5461a9d8aecf85144a04e2b6
Zoe Oliver <zoe@daleoliver.co.uk>
2017-11-28 10:55:01 +0000

    add linting

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ app.use('/*', (req, res, next) => {
     res.status(404).send({msg: "Page not found"});
   })
   
-  app.use('/*', (err, req, res, next) => {
-    if(err.type === 404) return res.status(404).send({msg: err.msg})
-    next(err);
-  })
-  
-  app.use((err, req, res, next) => {
-    res.status(500).send({err});
-  })
+app.use('/*', (err, req, res, next) => {
+  if(err.type === 404) return res.status(404).send({msg: err.msg})
+  next(err);
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).send({err});
+})
 
 module.exports = app;
